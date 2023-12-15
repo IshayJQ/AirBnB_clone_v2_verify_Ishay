3efe857f44430e299360b4ff718689d4cad2aaeb
Ishay Junnco <6905@holbertonstudents.com>
2023-12-15 12:28:00 -0800

    correction task one

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
+"""This module print the message"""
 from flask import Flask
 
 app = Flask(__name__)
+
+"""Return the message Hello·HBNB"""
 
 
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
+"""Return the message HBNB"""
 
 
 @app.route("/hbnb", strict_slashes=False)
