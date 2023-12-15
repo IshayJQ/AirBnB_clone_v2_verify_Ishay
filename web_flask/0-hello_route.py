20d025967d951072c207495b243e83dd38d59fbc
Ishay Junnco <6905@holbertonstudents.com>
2023-12-15 12:14:36 -0800

    correction task cero

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 """Return the message"""
-  @app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
