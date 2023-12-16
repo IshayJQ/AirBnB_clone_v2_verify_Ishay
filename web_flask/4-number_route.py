9390a860967e6b466e36b7bd1f67d162d8de24a5
Ishay Junnco <6905@holbertonstudents.com>
2023-12-15 20:07:48 -0800

    correction task four

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 
+"""This module 4-number_route.py print the message"""
 from flask import Flask
 
 app = Flask(__name__)
 
+"""Return the message Hello HBNB"""
+
 
 @app.route("/", strict_slashes=False)
 def hello():
-    return "Hello HBNB!\n"
-	
+    return "Hello HBNB!"
+
+"""Return the message HBNB"""
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return "HBNB!\n"
+    return "HBNB!"
+
+"""Return·the·message replace text"""
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def displaytext(text):
-		return f"c {text.replace('_', ' ')}\n"
+    return f"c {text.replace('_', ' ')}"
+
+"""Return·the·message replace text or not"""
+
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python/", strict_slashes=False)
 def displayPythonText(text='is_cool'):
-		return f"python {text.replace('_', ' ')}\n"
+    return f"python {text.replace('_', ' ')}"
+
+"""Return·the·message if is int"""
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def displayNumber(n):
-			return f"{n} is a number\n"
+    return f"{n} is a number"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True )
+    app.run(host='0.0.0.0', port=5000, debug=True)
