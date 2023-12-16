e9698e846fbbc696c8d315a11b2d27d5077636b1
Ishay Junnco <6905@holbertonstudents.com>
2023-12-15 20:12:53 -0800

    correction task five

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,52 @@
 #!/usr/bin/python3
 
+"""This module 5-number_template.py print the message"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+"""Return the message Hello HBNB"""
+
 
 @app.route("/", strict_slashes=False)
 def hello():
-    return "Hello HBNB!\n"
-	
+    return "Hello HBNB!"
+
+"""Return the message HBNB"""
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return "HBNB!\n"
+    return "HBNB!"
+
+"""Return·the·message replace text"""
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def displaytext(text):
-		return f"c {text.replace('_', ' ')}\n"
+    return f"c {text.replace('_', ' ')}"
+
+"""Return·the·message replace text or not"""
+
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python/", strict_slashes=False)
 def displayPythonText(text='is_cool'):
-		return f"python {text.replace('_', ' ')}\n"
+    return f"python {text.replace('_', ' ')}"
+
+"""Return·the·message if is int"""
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def displayNumber(n):
-			return f"{n} is a number\n"
+    return f"{n} is a number"
+
+"""Return·number template if is int"""
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def displayNumberTemplate(n):
-			return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True )
+    app.run(host='0.0.0.0', port=5000, debug=True)
